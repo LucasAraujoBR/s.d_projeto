e7c8be78c96752479adbc49b3d5de8b3e5394b78
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2023-04-03 14:09:49 -0300

    consumo api django

---
+++ java/costa.mar/src/main/java/com/hotel/costa/mar/model/dto/Usuario.java
@@ -0,0 +1,12 @@
+package com.hotel.costa.mar.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Usuario {
+    private String email;
+}
